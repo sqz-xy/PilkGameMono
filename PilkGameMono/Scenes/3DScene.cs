cd9c615391287a860a28b45bc52cbf38aba6cdc1
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2023-09-02 15:50:56 +0100

    spinning cube

--- PilkGameMono/Scenes/3DScene.cs
+++ PilkGameMono/Scenes/3DScene.cs
@@ -7,6 +7,7 @@ using PilkEngineMono.Scenes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,10 +24,7 @@ namespace PilkGameMono.Scenes
         Matrix viewMat;
         Matrix worldMat;
 
-        BasicEffect basicEffect;
-
-        VertexPositionColor[] triangleVertices;
-        VertexBuffer vertexBuffer;
+        Model model;
 
         bool orbit = true;
 
@@ -43,46 +41,25 @@ namespace PilkGameMono.Scenes
         public override void Initialize()
         {
             camTarget = new Vector3(0f, 0f, 0f);
-            camPos = new Vector3(0f, 0f, -100f);
-            projMat = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45f),mSceneManager.GraphicsDevice.DisplayMode.AspectRatio,1f, 1000f);
-            viewMat = Matrix.CreateLookAt(camPos, camTarget,new Vector3(0f, 1f, 0f));// Y up
-            worldMat = Matrix.CreateWorld(camTarget, Vector3.Forward, Vector3.Up);
+            camPos = new Vector3(0f, 0f, 20f);
+            projMat = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 480f, 0.1f, 100f);
+            viewMat = Matrix.CreateLookAt(new Vector3(0, 0, 10), new Vector3(0, 0, 0), Vector3.UnitY);
+            worldMat = Matrix.CreateTranslation(new Vector3(0, 0, 0));
 
-            //BasicEffect
-            basicEffect = new BasicEffect(mSceneManager.GraphicsDevice);
-            basicEffect.Alpha = 1f;
-
-            basicEffect.LightingEnabled = false;
-     
-            //Geometry  - a simple triangle about the origin
-            triangleVertices = new VertexPositionColor[3];
-            triangleVertices[0] = new VertexPositionColor(new Vector3(0, 20, 0), Color.Red);
-            triangleVertices[1] = new VertexPositionColor(new Vector3(-20, -20, 0), Color.Green);
-            triangleVertices[2] = new VertexPositionColor(new Vector3(20, -20, 0), Color.Blue);
-
-            //Vert buffer
-            vertexBuffer = new VertexBuffer(mSceneManager.GraphicsDevice, typeof( VertexPositionColor), 3, BufferUsage.WriteOnly);
-            vertexBuffer.SetData<VertexPositionColor>(triangleVertices);
+            model = mSceneManager.Content.Load<Model>("cube");
         }
 
         public override void Render()
         {
-            basicEffect.Projection = projMat;
-            basicEffect.View = viewMat;
-            basicEffect.World = worldMat;
-
-            mSceneManager.GraphicsDevice.Clear(Color.CornflowerBlue);
-            mSceneManager.GraphicsDevice.SetVertexBuffer(vertexBuffer);
-
-            RasterizerState rasterizerState = new RasterizerState();
-            rasterizerState.CullMode = CullMode.None;
-            mSceneManager.GraphicsDevice.RasterizerState = rasterizerState;
-
-            foreach (EffectPass pass in basicEffect.CurrentTechnique.
-                    Passes)
+            foreach (ModelMesh mesh in model.Meshes)
             {
-                pass.Apply();
-                mSceneManager.GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 3);
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = worldMat;
+                    effect.View = viewMat;
+                    effect.Projection = projMat;
+                }
+                mesh.Draw();
             }
         }
 
